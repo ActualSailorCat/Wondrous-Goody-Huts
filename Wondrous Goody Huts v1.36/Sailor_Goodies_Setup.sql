db52276c256e773f23c0ee96fa94330e86aabefa
ActualSailorCat <valkrana.miraquinal@gmail.com>
2021-04-23 16:19:46 -0400

    v1.36
    Fixed Improvements reward. Added SECRET WOAH

---
+++ Wondrous Goody Huts v1.36/Sailor_Goodies_Setup.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS SailorGoodyOptions (Type text default null);
+INSERT INTO SailorGoodyOptions (Type)
+VALUES ('CITYSTATE'), ('FORMATION'), ('POLICY'), ('RESOURCE'), ('SIGHT'), ('UI'), ('UU'), ('WONDER');
